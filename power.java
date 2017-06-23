d5e0e4a9332c21dc9371d41117e4f9872e8536aa
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-06-23 11:01:23 +0530

    Add files via upload

--- power.java
+++ power.java
@@ -1,5 +1,26 @@
 package Guvi;
 
 public class power {
+	public static void main(String ars[]){
+	int n=5,p=3,result=1;
 
+    if(n>=0&&p==0)
+     {
+        result=1;
+     }
+    else if(n==0&&p>=1)
+      { 
+         result=0;
+      }
+    else
+     {
+         for(int i=1;i<=p;i++)
+	 {
+            result=result*n;
+ 	 }	    
+     }
+    System.out.println(n+"^"+p+"="+result);
+	
+  }
 }
+
