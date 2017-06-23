0439b86407b3454b18e5257336b501beb97ef2ab
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-06-23 11:08:46 +0530

    Add files via upload

---
+++ Primenumbers.java
@@ -0,0 +1,14 @@
+package Guvi;
+
+public class Primenumbers {
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=2;i++){//loop for take 1 to 100 numbers
+		if(i%2!=0)//condition to find odd numbers between 1 to 100
+		{
+		System.out.println(i);// print odd values between 1 to 100
+		}
+		}
+		}
+		}
+
