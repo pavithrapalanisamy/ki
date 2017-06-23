6a2be0a7c5a1adb0a607e8f4eee82c0f6c9bb3a5
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-06-23 11:11:00 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,15 @@
+package Guvi;
+
+public class Even {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int start = 100;
+		int end = 200;
+
+		for(int i = start; i <=end; i++){
+		if( i % 2 == 0 ){
+		System.out.println("even num "+i);
+		}
+		}
+	}
+}
